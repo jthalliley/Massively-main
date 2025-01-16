26b23dec32eb4581342efb42544a6a6d2b77324f
Tom Halliley <jthalliley@gmail.com>
2025-01-16 07:51:59 -0500

    Upgraded to newer sass standards

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const pump = require('pump');
 
 // gulp plugins and utils
 var livereload = require('gulp-livereload');
-var sass = require('gulp-sass')(require('node-sass'));
+var sass = require('gulp-sass')(require('sass'));
 var zip = require('gulp-zip');
 var beeper = require('beeper');
 
@@ -28,7 +28,7 @@ function hbs(done) {
     ], handleError(done));
 }
 
-sass.compiler = require('node-sass');
+sass.compiler = require('sass');
 
 function css(done) {
     pump([
